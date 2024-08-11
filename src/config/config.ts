27ca6a2e98ee28a1241b4f2f13f7de76b310ebc2
Cheetah0x <niallcheetham@gmail.com>
2024-08-11 12:25:49 +0100

    site url

--- src/config/config.ts
+++ src/config/config.ts
@@ -8,4 +8,5 @@ const { useGlobalState } = createGlobalState(initialState);
 
 export { useGlobalState };
 
-export const NEXT_PUBLIC_URL = "http://localhost:3000";
+// export const NEXT_PUBLIC_URL = "http://localhost:3000";
+export const NEXT_PUBLIC_URL = "https://user-attest-module.vercel.app";
