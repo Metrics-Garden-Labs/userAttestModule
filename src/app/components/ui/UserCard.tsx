c7d0d7923fc1ae414815de19f4908dfa51735ddd
dolvin17 <huertas876@gmail.com>
2024-10-24 07:46:32 +0200

    newmodalv.0.1

--- src/app/components/ui/UserCard.tsx
+++ src/app/components/ui/UserCard.tsx
@@ -4,19 +4,19 @@ import { Users } from "../../../../src/lib/utils/types";
 
 interface UserCardProps {
   user: Users;
+  onSelect: () => void;
 }
 
-function UserCard({ user }: UserCardProps) {
+function UserCard({ user, onSelect }: UserCardProps) {
   return (
-    <div className="flex flex-col p-6 border justify-center items-center bg-white text-black border-gray-300 rounded-2xl w-full h-72 shadow-lg cursor-pointer hover:shadow-xl transition-shadow duration-300">
-      <div className="w-36 h-36 rounded-full overflow-hidden mb-4 relative">
+    <div className="flex flex-col p-6 border justify-center items-center bg-white text-black border-gray-300 rounded-md w-full h-72 shadow-xl">
+      <div className="w-36 h-36 shrink-0 rounded-full overflow-hidden mb-4 relative">
         {user.image ? (
-          <Image 
-            src={user.image} 
+          <Image
+            src={user.image}
             alt={`${user.name}'s profile picture`}
-            layout="fill"
-            objectFit="cover"
-            className="rounded-full"
+            className="size-full object-cover"
+			fill
           />
         ) : (
           <div className="w-full h-full flex items-center justify-center bg-gray-200 text-gray-500">
@@ -25,26 +25,29 @@ function UserCard({ user }: UserCardProps) {
         )}
       </div>
       <div className="flex items-center justify-center mb-2">
-      <h3 className=" text-xl font-semibold text-center">
-        {user.username}
-      </h3>
-      {user.verified && (
-        <Image
-          src="/githubverified.png"
-          alt="Verified"
-          width={24}
-          height={24}
-          className="inline-block ml-2"
-          title="GitHub Verified"
-        />
-
-      )}
+        <h3 className=" text-xl font-semibold text-center">{user.username}</h3>
+        {user.verified && (
+          <Image
+            src="/githubverified.png"
+            alt="Verified"
+            width={24}
+            height={24}
+            className="inline-block ml-2 shrink-0"
+            title="GitHub Verified"
+          />
+        )}
       </div>
       {user.bio && (
         <p className="text-sm text-gray-500 text-center overflow-hidden text-ellipsis">
           {user.bio.length > 50 ? `${user.bio.substring(0, 50)}...` : user.bio}
         </p>
       )}
+      <button
+        onClick={onSelect}
+        className="btn bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded-lg"
+      >
+        Review
+      </button>
     </div>
   );
 }
--- src/app/searchUsers/userList.tsx
+++ src/app/searchUsers/userList.tsx
@@ -1,12 +1,12 @@
 //displays a list of users based on the search query and filters
 
-'use client';
+"use client";
 
-import React, { useEffect, useMemo, useState } from 'react';
-import UserCard from '../components/ui/UserCard';
-import UserModal from '../components/ui/UserModal';
-import { Users } from '../../lib/utils/types';
-import { useSwitchChain } from 'wagmi';
+import React, { useEffect, useMemo, useState } from "react";
+import UserCard from "../components/ui/UserCard";
+import UserModal from "../components/ui/UserModal";
+import { Users } from "../../lib/utils/types";
+import { useSwitchChain } from "wagmi";
 
 interface UserListProps {
   users: Users[];
@@ -15,20 +15,27 @@ interface UserListProps {
   verificationFilter: string;
 }
 
-export default function UserList({ users, query, filter, verificationFilter }: UserListProps) {
+export default function UserList({
+  users,
+  query,
+  filter,
+  verificationFilter,
+}: UserListProps) {
   const [selectedUser, setSelectedUser] = useState<Users | null>(null);
   const { switchChain } = useSwitchChain();
 
   useEffect(() => {
-    switchChain({ chainId: 11155420});
+    switchChain({ chainId: 11155420 });
     console.log("Switched to chain 11155420");
-   }, []);
+  }, []);
 
   const filteredUsers = useMemo(() => {
     return users.filter((user) => {
-      const matchesQuery = user.username.toLowerCase().includes(query.toLowerCase());
+      const matchesQuery = user.username
+        .toLowerCase()
+        .includes(query.toLowerCase());
       // Note: Adjust this filtering logic based on your actual verification criteria
-      const matchesVerification = verificationFilter === '';
+      const matchesVerification = verificationFilter === "";
       return matchesQuery && matchesVerification;
     });
   }, [users, query, verificationFilter]);
@@ -43,11 +50,13 @@ export default function UserList({ users, query, filter, verificationFilter }: U
 
   return (
     <div className="p-6 bg-white mx-auto gap-12 max-w-6xl">
-      <div className="grid grid-cols-1 gap-4 mx-3 sm:grid-cols-2 sm:gap-4 sm:mx-3 md:grid-cols-3 md:mx-8 lg:grid-cols-4 lg:gap-12 max-w-6xl overflow-y-auto">
+      <div className="grid grid-cols-1 gap-4 mx-3 sm:grid-cols-2 sm:gap-4 sm:mx-3 md:grid-cols-3 md:mx-8 lg:grid-cols-4 lg:gap-12 max-w-6xl">
         {filteredUsers.map((user) => (
-          <div key={user.id} onClick={() => handleUserClick(user)}>
-            <UserCard user={user} />
-          </div>
+          <UserCard
+            key={`user.id-${user.id}`}
+            user={user}
+            onSelect={() => handleUserClick(user)}
+          />
         ))}
       </div>
       {selectedUser && (
